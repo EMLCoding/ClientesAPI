ec6d2462b85cc2a210439c0f707b6bf3074694d5
Edu-Estudios <eduardo.ml.trabajo@gmail.com>
2022-07-11 18:27:07 +0200

    Se añade el metodo para hacer el update

--- src/main/java/com/emlcoding/springboot/backend/apirest/controllers/ClienteController.java
+++ src/main/java/com/emlcoding/springboot/backend/apirest/controllers/ClienteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,12 +38,26 @@ public class ClienteController {
 	}
 	
 	@PostMapping("/cliente")
-	@ResponseStatus(HttpStatus.CREATED)
+	@ResponseStatus(HttpStatus.OK)
 	public Cliente createCliente(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
 	}
 	
+	@PutMapping("/cliente/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable UUID id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		// Como el cliente tiene un ID entonces el .save hace un update en la bbdd
+		return clienteService.save(clienteActual);
+	}
+	
 	@DeleteMapping("/cliente")
+	@ResponseStatus(HttpStatus.OK)
 	public void deletecliente(@PathVariable UUID id) {
 		clienteService.delete(id);
 	}
